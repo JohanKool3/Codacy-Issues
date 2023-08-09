a8941495c868f3ebddb54b10d1a4db78fa756771
Ecstatik <johankool2003@hotmail.co.uk>
2023-08-09 16:53:48 +0100

    Added an async method that returns void

---
+++ Codacy Issues/Performance/AsyncReturnVoid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codacy_Issues.Performance
+{
+    internal class AsyncReturnVoid
+    {
+
+        public async void DoSomethingAsync()
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
