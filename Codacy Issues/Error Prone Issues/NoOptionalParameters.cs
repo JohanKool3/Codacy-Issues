93f483adf06069c84ca162a0bfae8a221b34c016
Ecstatik <johankool2003@hotmail.co.uk>
2023-08-09 16:38:13 +0100

    Removed unused usings

--- Codacy Issues/Error Prone Issues/NoOptionalParameters.cs
+++ Codacy Issues/Error Prone Issues/NoOptionalParameters.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Codacy_Issues.Error_Prone_Issues
 {
     public class NoOptionalParameters
