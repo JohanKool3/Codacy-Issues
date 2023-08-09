ce48e05802b04c2dbe2cf88bf36642632390b7b6
Ecstatik <johankool2003@hotmail.co.uk>
2023-08-09 15:55:03 +0100

    Unused parameter issue example

---
+++ Codacy Issues/Unused Code Issues/UnusedMethodParameters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codacy_Issues.Unused_Code_Issues
+{
+    internal class UnusedMethodParameters
+    {
+        private void DoSomething(int a, int b, int c)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public void RunApplication(int a, int b, int c)
+        {
+            Console.WriteLine("Currently Running Application");
+            Console.WriteLine(a + b);
+        }
+    }
+}
